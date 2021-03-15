de26281722b2b7b6938f2e0a2a1db8016e7fc991
Olivier Coudray <ocoudray@hotmail.fr>
2021-03-15 09:25:57 +0100

    Add authentication system

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('home/index.html.twig');
+    	
+    	$user = $userRepository;
+        return $this->render('home/index.html.twig', compact('user'));
     }
 }
