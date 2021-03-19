0b9e4037b3842b60ea3d6eda03a250ed46456e52
Olivier Coudray <ocoudray@hotmail.fr>
2021-03-19 08:43:18 +0100

    rename gravatar method to gravatarUrl

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -324,7 +324,7 @@ class User implements UserInterface
         return $this->getFirstName() . ' ' . $this->getLastName();
     }
 
-    public function gravatar(?int $size=150)
+    public function gravatarUrl(?int $size=100)
     {
        
         return 'https://www.gravatar.com/avatar/'. md5(strtolower(trim($this->getEmail()))) .'/?s='.$size;
